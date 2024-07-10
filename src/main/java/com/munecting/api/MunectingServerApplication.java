6178270d8d168912a2bbb760e42b91aca18ca150
mingmingmon <mingmingmon@naver.com>
2024-07-11 08:33:27 +0900

    setting: 스프링부트 세팅 #1

---
+++ src/main/java/com/munecting/api/MunectingServerApplication.java
@@ -0,0 +1,13 @@
+package com.munecting.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunectingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunectingServerApplication.class, args);
+	}
+
+}
