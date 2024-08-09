691e567a5025868a60ef77a01e2ec76662253885
mingmingmon <mingmingmon@naver.com>
2024-08-09 11:03:17 +0900

    fix: CommentRequestDto 어노테이션 수정 #10

--- src/main/java/com/munecting/api/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/munecting/api/domain/comment/dto/CommentRequestDto.java
@@ -1,10 +1,8 @@
 package com.munecting.api.domain.comment.dto;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
-@Builder
 @Getter
 @Setter
 public class CommentRequestDto {
