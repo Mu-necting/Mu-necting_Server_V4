88396087d19f1c4fc6a1349f1c465fe3ea2e058b
esfuerzo <dkdldrha@naver.com>
2024-08-10 10:51:27 +0900

    OAuth2
    OAuth2 설정

---
+++ src/main/java/com/munecting/api/global/oauth2/dto/SpotifyResponse.java
@@ -0,0 +1,36 @@
+package com.munecting.api.global.oauth2.dto;
+
+import com.munecting.api.domain.user.dto.Role;
+import com.munecting.api.domain.user.dto.SocialType;
+import com.munecting.api.domain.user.entity.User;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class SpotifyResponse implements OAuth2Response{
+    private final Map<String, Object> attribute;
+
+    public SpotifyResponse(Map<String, Object> attribute) {
+        this.attribute = attribute;
+    }
+
+    @Override
+    public String getProviderId() {
+        return attribute.get("id").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        return attribute.get("email").toString();
+    }
+
+    @Override
+    public User toEntity(OAuth2Response oAuth2Response, SocialType socialType) {
+        return User.builder()
+                .socialType(socialType)
+                .email(oAuth2Response.getProviderId())
+                .nickname(String.valueOf(UUID.randomUUID()).substring(0, 8))
+                .role(Role.USER)
+                .build();
+    }
+}
