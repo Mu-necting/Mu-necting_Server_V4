c7aeb1a469ee16309764ef8cd52a69e82c9afd0a
mungsil <thddmd2009@gmail.com>
2024-08-25 00:10:50 +0900

    feat: 좋아요와 댓글 조회 API 응답에 좋아요 여부 추가 #29

--- src/main/java/com/munecting/api/domain/like/service/LikeService.java
+++ src/main/java/com/munecting/api/domain/like/service/LikeService.java
@@ -1,7 +1,26 @@
 package com.munecting.api.domain.like.service;
 
+import com.munecting.api.domain.like.dao.LikeRepository;
+import com.munecting.api.domain.like.dto.response.AddTrackLikeResponseDto;
+import com.munecting.api.domain.like.dto.response.DeleteTrackLikeResponseDto;
+import com.munecting.api.domain.like.dto.response.GetLikedTrackResponseDto;
+import com.munecting.api.domain.like.entity.Like;
+import com.munecting.api.domain.spotify.service.SpotifyService;
+import com.munecting.api.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class LikeService {
+
+    private final LikeRepository likeRepository;
+
+    public boolean isTrackLikedByUser(String trackId, Long userId) {
+        return likeRepository.existsByUserIdAndTrackId(userId, trackId);
+    }
+
 }
--- src/main/java/com/munecting/api/domain/track/dto/response/GetTrackDetailsResponseDto.java
+++ src/main/java/com/munecting/api/domain/track/dto/response/GetTrackDetailsResponseDto.java
@@ -4,11 +4,13 @@ import lombok.Builder;
 
 @Builder
 public record GetTrackDetailsResponseDto(
+        boolean isLiked,
         int likeCount,
         int commentCount
 ) {
-    public static GetTrackDetailsResponseDto of (int likeCount, int commentCount) {
+    public static GetTrackDetailsResponseDto of (boolean isLiked, int likeCount, int commentCount) {
         return GetTrackDetailsResponseDto.builder()
+                .isLiked(isLiked)
                 .likeCount(likeCount)
                 .commentCount(commentCount)
                 .build();
