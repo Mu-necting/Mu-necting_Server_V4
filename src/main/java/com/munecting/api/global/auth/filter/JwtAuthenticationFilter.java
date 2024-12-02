7bf6a40e777554969a333c83c4af11d8f1b37dff
mungsil <thddmd2009@gmail.com>
2024-12-02 21:03:58 +0900

    fix: 화이트리스트 요청은 토큰 검증 생략 #64

--- src/main/java/com/munecting/api/global/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/munecting/api/global/auth/filter/JwtAuthenticationFilter.java
@@ -1,8 +1,7 @@
 package com.munecting.api.global.auth.filter;
 
-import com.munecting.api.domain.user.dao.UserRepository;
 import com.munecting.api.global.auth.jwt.JwtProvider;
-import com.munecting.api.global.error.exception.UnauthorizedException;
+import com.munecting.api.global.util.AllowedPathPatternProvider;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,13 +11,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Optional;
-
-import static com.munecting.api.global.common.dto.response.Status.INVALID_TOKEN;
 
 @Slf4j
 @Component
@@ -26,37 +22,45 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
     private final String authHeader;
-    private final String prefix;
-    private final UserRepository userRepository;
+    private final AllowedPathPatternProvider allowedPathPatternProvider;
 
     public JwtAuthenticationFilter(
             JwtProvider jwtProvider,
             @Value("${spring.security.auth.header}") String authHeader,
-            @Value("${spring.security.auth.prefix}") String prefix,
-            UserRepository userRepository
+            AllowedPathPatternProvider allowedPathPatternProvider
     ) {
         this.jwtProvider = jwtProvider;
         this.authHeader = authHeader;
-        this.prefix = prefix;
-        this.userRepository = userRepository;
+        this.allowedPathPatternProvider = allowedPathPatternProvider;
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws
-            ServletException,
-            IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        if (isPathWhitelisted(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        processAuthentication(request);
+        filterChain.doFilter(request, response);
+    }
 
+    private boolean isPathWhitelisted(HttpServletRequest request) {
+        return allowedPathPatternProvider.isPathWhitelisted(request.getRequestURI());
+    }
+
+    private void processAuthentication(HttpServletRequest request) {
         Optional<String> bearerToken = Optional.ofNullable(request.getHeader(authHeader));
         bearerToken.ifPresent(it -> {
             String accessToken = jwtProvider.extractAccessToken(it);
             jwtProvider.validateAccessToken(accessToken);
             setAuthentication(accessToken);
         });
-        filterChain.doFilter(request, response);
     }
 
     private void setAuthentication(String accessToken) {
         UsernamePasswordAuthenticationToken authentication = jwtProvider.getAuthentication(accessToken);
         SecurityContextHolder.getContext().setAuthentication(authentication);
     }
+
 }
