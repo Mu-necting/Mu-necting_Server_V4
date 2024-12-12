f5acaba0ccf8d64497158f81080962ec02957387
mungsil <thddmd2009@gmail.com>
2024-12-12 19:20:14 +0900

    refactor: UserLike -> UserTrackLike 엔티티명 변경 #68

--- src/main/java/com/munecting/api/domain/like/entity/UserTrackLike.java
+++ src/main/java/com/munecting/api/domain/like/entity/UserTrackLike.java
@@ -1,10 +1,7 @@
 package com.munecting.api.domain.like.entity;
 
 import com.munecting.api.global.common.domain.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -13,7 +10,7 @@ import lombok.*;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class UserLike extends BaseEntity {
+public class UserTrackLike extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
