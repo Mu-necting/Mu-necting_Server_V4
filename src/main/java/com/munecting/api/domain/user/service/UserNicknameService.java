df8c4632601cd7eb1263fd5fd7134f7013e531d9
mungsil <thddmd2009@gmail.com>
2025-01-23 18:05:29 +0900

    fix: 랜덤 닉네임 구성 요소 및 난수 생성 구현체 변경 #71

--- src/main/java/com/munecting/api/domain/user/service/UserNicknameService.java
+++ src/main/java/com/munecting/api/domain/user/service/UserNicknameService.java
@@ -2,14 +2,13 @@ package com.munecting.api.domain.user.service;
 
 import com.munecting.api.domain.user.dao.UserRepository;
 import com.munecting.api.domain.user.entity.User;
-import com.munecting.api.global.error.exception.InternalServerException;
 import com.munecting.api.global.error.exception.InvalidValueException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import static com.munecting.api.global.common.dto.response.Status.*;
 
@@ -18,30 +17,58 @@ import static com.munecting.api.global.common.dto.response.Status.*;
 @RequiredArgsConstructor
 public class UserNicknameService {
 
-    private final UserRepository userRepository;
-
     private static final int MIN_LENGTH = 2;
     private static final int MAX_LENGTH = 15;
-
-    private static final String NAME_VALUE_RULES = "^(?=.*[a-zA-Z0-9가-힣])[a-zA-Z가-힣][a-zA-Z0-9가-힣_]*$";
-
+    private static final int FIXED_CHARS_LENGTH = 7;
+    private static final int SEVEN_DIGIT_MAX_NUM = 9_999_999;
+    private static final int EIGHT_DIGIT_MAX_NUM = 99_999_999;
     private static final String DEFAULT_NICKNAME = "뮤넥터";
-    private static final String DELIMITER = "_";
-    private static final int MAX_UNIQUE_STRING_LENGTH = 10;
-    private static final int MAX_NICKNAME_RETRY_COUNT = 100;
-    private static final String CHARACTERS_POOL = "0123456789abcdefghijklmnopqrstuvwxyz";
-    private final Random random = new Random();
+    private static final String NAME_VALUE_RULES = "^(?=.*[a-zA-Z0-9가-힣])[a-zA-Z가-힣][a-zA-Z0-9가-힣_]*$";
+    private static final String SPACE = " ";
+    private static final ThreadLocalRandom random = ThreadLocalRandom.current();
+    private static final String[] ADJECTIVES = {
+            "엉뚱한", "기묘한", "독창적인", "파격적인", "치명적인", "미묘한",
+            "경쾌한", "상쾌한", "즐거운", "신나는", "활기찬", "반짝이는",
+            "명랑한", "유쾌한", "대담한", "클래식한", "서정적인", "깜찍한", "대단한"
+    };
+
+    private final UserRepository userRepository;
+
+    public String generateUniqueNickname() {
+        String chars = generateRandomCharacter();
+        int num = generateRandomNumber(chars.length());
+
+        return chars + num;
+    }
 
     public String updateNickname(User user, String nickname) {
         if (!StringUtils.hasText(nickname)) {
             return user.getNickname();
         }
 
-        validateNickname(user, nickname);
+        validateNamePolicy(nickname);
+        validateDuplicatedNickname(user, nickname);
+
         return user.updateNickname(nickname);
     }
 
-    private void validateNickname(User existingUser, String nickname) {
+    private String generateRandomCharacter() {
+        int index = random.nextInt(ADJECTIVES.length);
+        String adjective = ADJECTIVES[index];
+
+        return adjective + SPACE + DEFAULT_NICKNAME;
+    }
+
+    private int generateRandomNumber(int charsLength) {
+        int remainLength = MAX_LENGTH - charsLength;
+        int maxNumber = remainLength == FIXED_CHARS_LENGTH
+                ? EIGHT_DIGIT_MAX_NUM
+                : SEVEN_DIGIT_MAX_NUM;
+
+        return random.nextInt(1, maxNumber + 1);
+    }
+
+    private void validateNamePolicy(String nickname) {
         if (isInvalidNicknameLength(nickname)) {
             throw new InvalidValueException(INVALID_NICKNAME_LENGTH);
         }
@@ -49,8 +76,10 @@ public class UserNicknameService {
         if (isInvalidNamingRule(nickname)) {
             throw new InvalidValueException(INVALID_NICKNAME_VALUE);
         }
+    }
 
-        if (isDuplicatedNickname(existingUser, nickname)) {
+    private void validateDuplicatedNickname(User user, String nickname) {
+        if (isDuplicatedNickname(user, nickname)) {
             throw new InvalidValueException(DUPLICATED_NICKNAME);
         }
     }
@@ -72,31 +101,4 @@ public class UserNicknameService {
         return userRepository.existsByNickname(nickname);
     }
 
-    public String generateUniqueNickname() {
-        String nickname;
-        int attemptCount = 0;
-
-        do {
-            if (attemptCount >= MAX_NICKNAME_RETRY_COUNT) {
-                throw new InternalServerException("닉네임 생성에 실패하였습니다.");
-            }
-            nickname = DEFAULT_NICKNAME + DELIMITER + generateUniqueString();
-            attemptCount++;
-
-        } while (userRepository.existsByNickname(nickname));
-
-        return nickname;
-    }
-
-    private String generateUniqueString() {
-        int uniqueStringLength = random.nextInt(MAX_UNIQUE_STRING_LENGTH);
-        log.info("uniqueStringLength : {}", uniqueStringLength);
-
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < uniqueStringLength; i++) {
-            result.append(CHARACTERS_POOL.charAt(random.nextInt(CHARACTERS_POOL.length())));
-        }
-
-        return result.toString();
-    }
 }
